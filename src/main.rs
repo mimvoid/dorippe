ac4c48250b42df8f20b00da1e4603716f7d7b2cc
mimvoid <mimvoid@proton.me>
2025-07-10 14:28:47 -0400

    Move widget components to separate files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod main_pane;
+mod sidebar;
+
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Box, Button, Label, Paned, glib};
+use gtk::{Application, ApplicationWindow, Paned, glib};
 
 fn main() -> glib::ExitCode {
     let app = Application::builder()
@@ -11,48 +14,11 @@ fn main() -> glib::ExitCode {
 }
 
 fn init_window(app: &Application) {
-    let sidebar = Box::new(gtk::Orientation::Vertical, 0);
-    {
-        let recent = Button::with_label("Recent");
-        let home = Button::with_label("Home");
-        sidebar.append(&recent);
-        sidebar.append(&home);
-    }
-
-    let main_pane = Box::new(gtk::Orientation::Vertical, 0);
-    {
-        let header = Box::new(gtk::Orientation::Horizontal, 2);
-
-        let go_back = Button::with_label("<");
-        let go_forward = Button::with_label(">");
-        header.append(&go_back);
-        header.append(&go_forward);
-
-        let file_listings = Box::builder()
-            .orientation(gtk::Orientation::Horizontal)
-            .spacing(2)
-            .halign(gtk::Align::Start)
-            .valign(gtk::Align::Start)
-            .hexpand(true)
-            .vexpand(true)
-            .build();
-
-        let button1 = Button::with_label("mock-file-1.txt");
-        let button2 = Button::with_label("mock-file-2.txt");
-        file_listings.append(&button1);
-        file_listings.append(&button2);
-
-        let status = Box::new(gtk::Orientation::Horizontal, 2);
-        let mock_count = Label::new(Some("2 files"));
-        status.append(&mock_count);
-
-        main_pane.append(&header);
-        main_pane.append(&file_listings);
-        main_pane.append(&status);
-    }
+    let sidebar_pane = sidebar::build_sidebar();
+    let main_pane = main_pane::build_main_pane();
 
     let content = Paned::builder()
-        .start_child(&sidebar)
+        .start_child(&sidebar_pane)
         .end_child(&main_pane)
         .build();
 
