b79532cc78520415ad8b1e236a0d394962c51513
mimvoid <mimvoid@proton.me>
2025-07-11 12:04:42 -0400

    Reduce filter repetition

--- src/files/filters.rs
+++ src/files/filters.rs
@@ -2,16 +2,15 @@ use gio::FileInfo;
 use glib::object::Cast;
 use gtk::CustomFilter;
 
+fn is_directory(obj: &glib::Object) -> bool {
+    let file = obj.downcast_ref::<FileInfo>().unwrap();
+    file.file_type() == gio::FileType::Directory
+}
+
 pub fn directory_filter() -> CustomFilter {
-    CustomFilter::new(move |obj| {
-        let file = obj.downcast_ref::<FileInfo>().unwrap();
-        file.file_type() == gio::FileType::Directory
-    })
+    CustomFilter::new(move |obj| is_directory(obj))
 }
 
 pub fn not_directory_filter() -> CustomFilter {
-    CustomFilter::new(move |obj| {
-        let file = obj.downcast_ref::<FileInfo>().unwrap();
-        file.file_type() != gio::FileType::Directory
-    })
+    CustomFilter::new(move |obj| !is_directory(obj))
 }
